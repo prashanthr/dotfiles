83778b1aa531eab86fce23bf9b2dc64ef2c61a14
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2019-09-29 15:31:45 -0700

    fix(macos): Fixed double quote chars (#4)

--- scripts/mac-os-defaults.sh
+++ scripts/mac-os-defaults.sh
@@ -7,11 +7,11 @@ defaults write com.apple.dock autohide -bool true
 # Save screenshots to the desktop
 SCREENSHOT_DIR="$HOME/Desktop/screenshots"
 mkdir $SCREENSHOT_DIR
-defaults write com.apple.screencapture location -string “$SCREENSHOT_DIR”
+defaults write com.apple.screencapture location -string "$SCREENSHOT_DIR"
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults write com.apple.screencapture type -string “png”
+defaults write com.apple.screencapture type -string "png"
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 # Disable the sound effects on boot
-sudo nvram SystemAudioVolume=” “
+sudo nvram SystemAudioVolume=" "
 unset SCREENSHOT_DIR
